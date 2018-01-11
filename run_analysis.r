74820cbb7288201320c65c365e1d5f8c32133b3e
rauf <34421640+raufkhan777@users.noreply.github.com>
2018-01-10 22:06:27 -0500

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,42 +1,66 @@
+#create a directory if not found, in which the downloded data will be stored
+if(!file.exists("./Assignment")){dir.create("./Assignment")}
 
-library(dplyr)
+#data set zip Url, from where the data will be downloded
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-# read train data
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+#download the zip file
+download.file(fileUrl,destfile="./Assignment/Dataset.zip")
 
-# read test data
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+# unzip the zip file to Assignment directory
+unzip(zipfile="./Assignment/Dataset.zip",exdir="./Assignment")
 
-# read data description
-variable_names <- read.table("./UCI HAR Dataset/features.txt")
+#reading test tables
+xtest<- read.table("./Assignment/UCI HAR Dataset/test/X_test.txt")
+ytest<- read.table("./Assignment/UCI HAR Dataset/test/y_test.txt")
+subject_test<- read.table("./Assignment/UCI HAR Dataset/test/subject_test.txt")
 
-# read activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+#reading train tables
+xtrain<- read.table("./Assignment/UCI HAR Dataset/train/X_train.txt")
+ytrain<- read.table("./Assignment/UCI HAR Dataset/train/y_train.txt")
+subject_train<- read.table("./Assignment/UCI HAR Dataset/train/subject_train.txt")
 
-# 1. Merges the training and the test sets to create one data set.
-X_total <- rbind(X_train, X_test)
-Y_total <- rbind(Y_train, Y_test)
-Sub_total <- rbind(Sub_train, Sub_test)
+#reading features vector
+features<- read.table("./Assignment/UCI HAR Dataset/features.txt")
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
-X_total <- X_total[,selected_var[,1]]
+#reading the activity labels
+Activity_labels<- read.table("./Assignment/UCI HAR Dataset/activity_labels.txt")
 
-# 3. Uses descriptive activity names to name the activities in the data set
-colnames(Y_total) <- "activity"
-Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
-activitylabel <- Y_total[,-1]
+#renaming the colums
+colnames(xtest)<- features[,2]
+colnames(ytest)<- "ActivityID"
+colnames(subject_test)<- "subjectID"
+colnames(xtrain)<- features[,2]
+colnames(ytrain)<- "ActivityID"
+colnames(subject_train)<- "subjectID"
+colnames(Activity_labels)<- c("ActivityID", "ActivityName")
 
-# 4. Appropriately labels the data set with descriptive variable names.
-colnames(X_total) <- variable_names[selected_var[,1],2]
+#merging the test tables
+mergetest<- cbind(ytest, subject_test, xtest)
 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average
-# of each variable for each activity and each subject.
-colnames(Sub_total) <- "subject"
-total <- cbind(X_total, activitylabel, Sub_total)
-total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
-write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+#merging the train tables
+mergetrain<- cbind(ytrain, subject_train, xtrain)
+
+#merging the test and train rows
+allinone<- rbind(mergetrain, mergetest)
+
+#extracting the measurments of the mean and standard deviation
+colNames<- colnames(allinone)
+meanandstd<- grepl("ActivityID", colNames)|
+grepl("subjectID", colNames)|
+grepl("mean..", colNames)|
+grepl("std..", colNames)
+
+#New data with only the measurments of the mean and standard deviation
+setmeanandstd<- allinone[, meanandstd==TRUE]
+
+#assiging a activity names and ordering the data set 
+setactivitynames<- merge(Activity_labels,setmeanandstd, by='ActivityID', all.x=TRUE)
+setactivitynamesordered<- setactivitynames[order(setactivitynames$subjectID, setactivitynames$ActivityID),]
+
+#creating independent tidy data set with the average of each variable for each activity and each subject.
+dataset<- aggregate(. ~subjectID + ActivityName, setactivitynamesordered, mean)
+datasetordered<- dataset[order(dataset$subjectID, dataset$ActivityID),]
+
+#writing the tidy data
+write.table(datasetordered, "datasetordered.txt", row.name=FALSE
